b55b52cd57dc480a166456ce51abbc55b04a1883
Artem <artem.barabolenko@gmail.com>
2018-02-17 00:21:28 +0300

    added Train Class

--- src/main/java/com/artbrb/timetable/Train.java
+++ src/main/java/com/artbrb/timetable/Train.java
@@ -1,4 +1,36 @@
 package com.artbrb.timetable;
 
 public class Train {
+    private String name;
+    private String departureTime;
+    private String arrivalStation;
+
+    public Train (String name, String departureTime, String arrivalStation) {
+        this.name = name;
+        this.departureTime = departureTime;
+        this.arrivalStation = arrivalStation;
+    }
+
+    public String getArrivalStation() {
+        return arrivalStation;
+    }
+    public void setArrivalStation(java.lang.String arrivalStation) {
+        this.arrivalStation = arrivalStation;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String getDepartureTime() {
+        return departureTime;
+    }
+    public void setDepartureTime(String departureTime) {
+        this.departureTime = departureTime;
+    }
 }
--- src/main/java/com/artbrb/timetable/TrainTimetable.java
+++ src/main/java/com/artbrb/timetable/TrainTimetable.java
@@ -1,4 +1,30 @@
 package com.artbrb.timetable;
 
+import java.time.Instant;
+import java.util.List;
+
 public class TrainTimetable {
+
+    public List<Train> trains;
+
+    public void addNewTrain(String trainName, String departureTime, String arrivalStation) {
+
+    }
+
+    public void deleteTrain(String trainName) {
+
+    }
+
+    public void addIntermediateSttion(String trainName, String stationName) {
+
+    }
+
+    public void deleteIntermediateStation(String trainName, String stationName) {
+
+    }
+
+    public void findNearestTrain(String stationName, Instant time) {
+
+    }
+
 }
